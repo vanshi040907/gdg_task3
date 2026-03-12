266449e95d322f131fb01f68da72d51e62881f06
vanshi040907 <vanshi040907@gmail.com>
2026-03-12 19:14:21 +0530

    xyz

---
+++ frontend/photo_gallery/src/components/image_box.jsx
@@ -0,0 +1,10 @@
+function Image_box(){
+    return (
+        <div className="container">
+            <img src="."/>
+            <div className="status_area">
+
+            </div>
+        </div>
+    )
+}
