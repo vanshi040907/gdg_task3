819fb89b8c80d7a818b4e3ab86f7c52d04d31e3f
vishruthi25001079 <cse250001079@iiti.ac.in>
2026-03-18 17:04:05 +0530

    updated

--- frontend/photo_gallery/src/App.jsx
+++ frontend/photo_gallery/src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css'
+import './Header.css'
 import Image_box from "./components/image_box.jsx"
+import Header from "./Header.jsx"
+
 
 function App() {
   return (
+    <>
+      <Header />
       <Image_box />
+
+    </>
   )
 }
 
---
+++ frontend/photo_gallery/src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='logo'>
+                <div className='name'>
+                    Gallery
+                </div>
+                <div className='tag'>capturing memories</div>
+
+            </div>
+            <div className='login'> log in </div>
+        </div>
+    )
+}
+
+export default Header
