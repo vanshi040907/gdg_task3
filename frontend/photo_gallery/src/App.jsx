a63fade8aad68d31cf4d1827fca8899d725687ce
vanshi040907 <vanshi040907@gmail.com>
2026-03-19 10:28:28 +0530

    vjddcj

--- frontend/photo_gallery/src/App.jsx
+++ frontend/photo_gallery/src/App.jsx
@@ -3,7 +3,6 @@ import './Header.css'
 import Image_box from "./components/image_box.jsx"
 import Header from "./Header.jsx"
 
-
 function App() {
   return (
     <>
--- frontend/photo_gallery/src/components/image_box.jsx
+++ frontend/photo_gallery/src/components/image_box.jsx
@@ -1,15 +1,16 @@
-import "../App.css"
-function Image_box(){
+import "../css/image_box.css"
+function Image_box(x){
+    function onFavouriteClick(){
+        alert("clicked");
+    }
+
     return (
         <div className="container">
             <div className="image_area">
                 <img className="image" src={"/IMAGE_11.jpeg"}></img>
-            </div>
-            <div className="status">
-                <button className="like">❤️</button>
-                <span className="like_count">34</span>
-                <button className="upvote">👍</button>
-                <span className="upvote_count">23</span>
+                <div className="favorite-overlay">
+                    <button className="favourite-btn" onClick={onFavouriteClick}>❤️</button>
+                </div>
             </div>
         </div>
     )
