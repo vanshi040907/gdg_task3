f1a5fe1a50a4683a99272ee0f249ed443455b9ff
Saumya Roy <roysaumya767@gmail.com>
2026-03-25 10:26:03 +0530

    Update backend logic and package files

--- backend/controllers/photo.js
+++ backend/controllers/photo.js
@@ -2,19 +2,43 @@ const Photo = require('../models/photo');
 const User= require('../models/user');
 const Upvote= require('../models/upvote');
 
+const cloudinary = require("cloudinary").v2;
+
+  cloudinary.config({ 
+        cloud_name: 'dkau0yjwe', 
+        api_key: '815865897166141', 
+        api_secret: 'IQ21_FD-K_5CjPqYwG18eNyXZR8' 
+    });
+
+
+
 async function handlephotoupload(req,res) {
-  
+  try{
     const filename = req.file.filename;
-    await Photo.create ({
+    const filepath = req.file.path;
+    const cloudinaryresponse = await cloudinary.uploader.upload(filepath,{
+        folder:"cloud_folder"
+    })
+    console.log(cloudinaryresponse);
+    /*await Photo.create ({
         photofilename: filename,
+    
+    });*/
+    await Photo.create ({
+        photofilename:cloudinaryresponse.secure_url ,
     
     });
 
    
-    req.io.emit('photofile',filename);
+    req.io.emit('photofile',cloudinaryresponse.secure_url );
    
      
     return res.redirect("/");
+}
+catch (error) {
+        console.error("Upload failed:", error);
+        return res.status(500).send("Upload Error");
+    }
 }
 async function handlephotoshow(req,res) {
     
